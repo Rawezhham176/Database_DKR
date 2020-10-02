6f298e218cdbdef4e00c5288ac2717f9e58e932e
Rawezhham176 <rawezh.hama@otto.de>
2020-10-02 08:19:57 +0200

    KundenDaten

---
+++ KundenDaten.sql
@@ -0,0 +1,17 @@
+KundenNR,Nachname,Vorname,Geburtsdatum,Strasse,Hausnr,PLZ,Ort,Telefon,Email,Allergene
+2001,Wellensteyn,Kira,1990-05-05,Eppendorfer Landstrasse,104,20249,Hamburg,040/443322,k.wellensteyn@yahoo.de,Nusse
+2002,Foede,Dorothea,2000-03-24,Ohmstraße,23,22765,Hamburg,040/543822,d.foede@web.de,
+2003,Leberer,Sigrid,1989-09-21,Bilser Berg,6,20459,Hamburg,0175/1234588,sigrid@leberer.de,
+2004,Soerensen,Hanna,1974-04-03,Alter Teichweg,95,22049,Hamburg,040/634578,h.soerensen@yahoo.de,Weizen
+2005,Schnitter,Marten,1964-04-17,Stübels,10,22835,Barsbüttel,0176/447587,schni_mart@gmail.com,
+2006,Maurer,Belinda,1978-09-09,Grotelertwiete,4a,21075,Hamburg,040/332189,belinda1978@yahoo.de,Weizen
+2007,Gessert,Armin,1978-01-29,Küstersweg,3,21079,Hamburg,040/67890,armin@gessert.de,
+2008,Haessig,Jean-Marc,1982-08-30,Neugrabener Bahnhofstraße,30,21149,Hamburg,0178-67013390,jm@haessig.de,
+2009,Urocki,Eric,1999-12-04,Elbchaussee,228,22605,Hamburg,0152-96701390,urocki@outlook.de,
+2010,Hamze,Malli,1980-09-13,Egrad,23,21481,Lauenburg,0178-87625483,hamzeMal98@yahoo.de,Laktose
+2011,Kazim,Ali,2002-03-24,Asche,11,21481,Lauenburg,0176-23487594,kazim345@kazemi.com,
+2012,Müller,Emelli,2000-01-04,Niemand,76c,21481,Lauenburg,0156-34982712,emelliMüller@gmail.de,
+2013,Rieck,Laura,1998-05-11,Ecken,56,21481,Lauenburg,04153/42554,rieck54La@lol.de,
+2014,Schneider,Jamie,1978-11-29,Kino,13,21502,Geesthacht,0177-12348787,schneiderte34@gmx.de,Laktose
+2015,Ketter,Jun,1990-12-01,Hamburger,7,21502,Geesthacht,04152/66987,Kettjun@gmail.de,
+2016,Leitenträger,Toby,1987-07-03,See,2-3,21079,Hamburg,040/0998787,Leitentoby76@gmx.de,
